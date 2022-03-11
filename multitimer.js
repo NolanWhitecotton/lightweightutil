87d59ea02c2d2e69678b6f8879ea41a85c3e5861
Nolan Whitecotton <nolanwhitecotton@gmail.com>
2022-03-11 17:04:18 -0600

    Multitimer: moved the start click event to the class
    The Timer class now handles adding the eventListener for the timer.

--- multitimer.js
+++ multitimer.js
@@ -93,133 +93,51 @@ class Timer{
 			}
 		});
 
-		//TODO startButton click event listner
-
+		this.startButton.addEventListener("click", function(){
+			if(_this.startButton.value == "start" || _this.startButton.value == "resume"){
+				
+				if(_this.startButton.value == "start"){
+					_this.duration=createDuration(_this.hInput.value, _this.mInput.value, _this.sInput.value).totalMS;
+					_this.timeDisplay.innerHTML = getDurationAsString(_this.duration);
+					_this.timeInput.hidden = true;
+					_this.timeDisplay.hidden = false;
+				}
+				
+				_this.startButton.value = "pause";
+				
+				_this.startDate = new Date();
+				_this.interval = setInterval(function(){//TODO this could be done in a batches, where you iterate through all the timers in a single interval, and then update the time to save on cpu time
+					let difference = new Date()-_this.startDate;
+					let timeRemaining = _this.duration-(difference+_this.msOffset);
+					
+					if(timeRemaining > 0){
+						_this.timeDisplay.innerHTML = getDurationAsString(timeRemaining);
+					} else {
+						_this.timeDisplay.innerHTML = "Timer over!";
+						_this.startButton.value = "reset";
+						clearInterval(_this.interval);
+					}
+				}, 100);
+			}else if(_this.startButton.value == "pause"){
+				_this.msOffset += new Date()-_this.startDate;
+				_this.startButton.value = "resume";
+				clearInterval(_this.interval);
+			}else if(_this.startButton.value == "reset"){
+	
+				_this.startButton.value = "start";
+				_this.msOffset = 0;
+				_this.startdate = null;
+				clearInterval(_this.interval);
+				_this.timeInput.hidden = false;
+				_this.timeDisplay.hidden = true;
+			}
+		});
 	}
 }
 
 //adds a timer to the dom
 function mt_addTimer(){
 	timers.push(new Timer());
-	
-	//timer div
-	/*let timerDiv = document.createElement("span");
-	document.getElementById("timers").appendChild(timerDiv);
-	
-	//the name of the timer
-	let timerName = document.createElement("input");
-	timerName.type = "text";
-	timerName.value = "Timer";
-	timerDiv.appendChild(timerName);
-	
-	//the timer delete button
-	let timerDelete = document.createElement("input");
-	timerDelete.type = "button";
-	timerDelete.value = "x";
-	timerDelete.addEventListener("click",function(){
-		if(timerPause.value == "start"){
-			mt_deleteTimer(this.parentNode);
-		}else{
-			timerPause.value = "start";
-			timerPause.msOffset = 0;
-			timerPause.startdate = null;
-			clearInterval(timerDiv.interval);
-			stoppedTimerInput.hidden = false;
-			runningTimerText.hidden = true;
-		}
-	});
-	timerDiv.appendChild(timerDelete);
-	
-	//line break
-	let timerBreak = document.createElement("br");
-	timerDiv.appendChild(timerBreak);
-	
-	//pause/start/reset/resume button
-	let timerPause = document.createElement("input");
-	timerPause.type = "button";
-	timerPause.value = "start";
-	timerPause.msOffset = 0;
-	timerPause.startdate = null;
-	timerPause.duration = 1000*60;
-	timerPause.addEventListener("click", function(){
-		if(timerPause.value == "start" || timerPause.value == "resume"){
-			
-			if(timerPause.value == "start"){
-				timerPause.duration=createDuration(hrInput.value, minInput.value, secInput.value).totalMS;
-				runningTimerText.innerHTML = getDurationAsString(timerPause.duration);
-				stoppedTimerInput.hidden = true;
-				runningTimerText.hidden = false;
-			}
-			
-			timerPause.value = "pause";
-			
-			timerPause.startDate = new Date();
-			timerDiv.interval = setInterval(function(){//TODO this could be done in a batches, where you iterate through all the timers in a single interval, and then update the time to save on cpu time
-				let difference = new Date()-timerPause.startDate;
-				let timeRemaining = timerPause.duration-(difference+timerPause.msOffset);
-				
-				if(timeRemaining > 0){
-					runningTimerText.innerHTML = getDurationAsString(timeRemaining);
-				} else {
-					runningTimerText.innerHTML = "Timer over!";
-					timerPause.value = "reset";
-					clearInterval(timerDiv.interval);
-				}
-			}, 100);
-		}else if(timerPause.value == "pause"){
-			timerPause.msOffset += new Date()-timerPause.startDate;
-			timerPause.value = "resume";
-			clearInterval(timerDiv.interval);
-		}else if(timerPause.value == "reset"){
-
-			timerPause.value = "start";
-			timerPause.msOffset = 0;
-			timerPause.startdate = null;
-			clearInterval(timerDiv.interval);
-			stoppedTimerInput.hidden = false;
-			runningTimerText.hidden = true;
-		}
-	});
-	timerDiv.appendChild(timerPause);
-	
-	//text that displays the current time left 
-	let runningTimerText = document.createElement("span");
-	runningTimerText.innerHTML = "";
-	runningTimerText.hidden = true;
-	timerDiv.appendChild(runningTimerText);
-	
-	//input field for the time
-	let stoppedTimerInput = document.createElement("span");
-	timerDiv.appendChild(stoppedTimerInput);
-	
-	let hrInput = document.createElement("input");
-	hrInput.type = "text";
-	hrInput.placeholder = "HH";
-	hrInput.style.width = "3ch";
-	stoppedTimerInput.appendChild(hrInput);
-	
-	let colon1 = document.createElement("span");
-	colon1.innerHTML = ":";
-	stoppedTimerInput.appendChild(colon1);
-	
-	let minInput = document.createElement("input");
-	minInput.type = "text";
-	minInput.placeholder = "MM";
-	minInput.style.width = "3ch";
-	minInput.value = 1;
-	stoppedTimerInput.appendChild(minInput);
-	
-	let colon2 = document.createElement("span");
-	colon2.innerHTML = ":";
-	stoppedTimerInput.appendChild(colon2);
-	
-	let secInput = document.createElement("input");
-	secInput.type = "text";
-	secInput.placeholder = "SS";
-	secInput.style.width = "3ch";
-	stoppedTimerInput.appendChild(secInput);
-	*/
-	
 	console.log("added timer.");
 }
 
