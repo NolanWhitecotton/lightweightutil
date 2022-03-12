77e87e8769cf77b435b66c4ffc6f9996b609748e
Nolan Whitecotton <nolanwhitecotton@gmail.com>
2022-03-11 21:49:34 -0600

    Multitimer: Refactor timer object to make the constructor more readable
    Moved the event listeners code outside of the constructor to make the constructor more compact.

--- multitimer.js
+++ multitimer.js
@@ -1,15 +1,15 @@
 console.log("multitimer loaded.");
 
 //adds 0s to the begining of a number such that the minimum width is width
-function leftPadNumber(num, width){
+function LeftZeroFillNumber(num, width){
 	let string = "" + num;
-	
+
 	let output = "";
 	for(let i=string.length;i<width;i++){
 		output += "0";
 	}
 	output += string;
-	
+
 	return output;
 }
 
@@ -20,55 +20,128 @@ function createDuration(h,m,s){
 
 //converts a duration in ms to a duration object
 function getDurationFromMS(ms){
-	let secConstant = 1000;
-	let minConstant = secConstant*60;
-	let hourConstant = minConstant*60;
+	let MS_PER_SEC = 1000;
+	let MS_PER_MIN = MS_PER_SEC*60;
+	let MS_PER_HOUR = MS_PER_MIN*60;
 
-	let hours = Math.floor(ms / hourConstant);
-	ms %= hourConstant;
-	
-	let minutes = Math.floor(ms / minConstant);
-	ms %= minConstant;
-	
-	let seconds = Math.floor(ms / secConstant);
-	ms %= secConstant;
-	
+	let hours = Math.floor(ms / MS_PER_HOUR);
+	ms %= MS_PER_HOUR;
+
+	let minutes = Math.floor(ms / MS_PER_MIN);
+	ms %= MS_PER_MIN;
+
+	let seconds = Math.floor(ms / MS_PER_SEC);
+	ms %= MS_PER_SEC;
+
 	return createDuration(hours, minutes, seconds);
 }
 
 //converts a duration in ms to a string in HH:MM:SS
 function getDurationAsString(duration){
 	let dur = getDurationFromMS(duration);
-	
-	let output = "";
-	output += leftPadNumber(dur.hours, 2);
+
+	let output = LeftZeroFillNumber(dur.hours, 2);
 	output += ":";
-	output += leftPadNumber(dur.minutes, 2);
+	output += LeftZeroFillNumber(dur.minutes, 2);
 	output += ":";
-	output += leftPadNumber(dur.seconds, 2);
-	
+	output += LeftZeroFillNumber(dur.seconds, 2);
+
 	return output;
 }
 
 var timers = [];//list of all the timers
 var timerTickInterval = null; //the interval set if any timers exist
 var timerRingInterval = null; //the interval to check if any alarms are ringing
-var ringingTimers = 0;
 
 class Timer{
+	constructor(){
+		//init element fragment
+		let element = timer_tmplt.content.cloneNode(true);
+		this.timer = element.querySelector(".timer");//the element that this timer object corresponds to
+
+		//add element to dom
+		document.getElementById("timers").appendChild(element);
+
+		//element references
+		this.xButton = this.timer.querySelector(".x-button");
+		this.startButton = this.timer.querySelector(".start-button");
+		this.timeDisplay = this.timer.querySelector(".time-display");
+		this.timeInput = this.timer.querySelector(".time-input");
+		this.hInput = this.timer.querySelector(".h-input");
+		this.mInput = this.timer.querySelector(".m-input");
+		this.sInput = this.timer.querySelector(".s-input");
+
+		//default variable values
+		this.msOffset = 0;
+		this.startDate = null;
+		this.duration = 1000*60;
+
+		//references to this object for eventlisteners
+		let _this = this;
+
+		//event listeners
+		this.xButton.addEventListener("click", function(){
+			_this.resetEvent(_this);
+		});
+
+		this.startButton.addEventListener("click", function(){
+			_this.startButtonEvent(_this);
+		});
+	}
+
+	//toggles between showing the time left and editing the duration
+	toggleEditMode(_this){
+		_this.timeInput.hidden = !_this.timeInput.hidden;
+		_this.timeDisplay.hidden = !_this.timeInput.hidden;
+	}
+
+	//function that is called when the x button is clicked
+	resetEvent(_this){
+		if(_this.startButton.value == "start"){
+			mt_deleteTimer(_this);
+		}else{
+			_this.startButton.value = "start";
+			_this.msOffset = 0;
+			_this.startDate = null;
+			_this.toggleEditMode(_this);
+		}
+	}
+
+	//function that is called when the start button is clicked
+	startButtonEvent(_this){
+		if(_this.startButton.value == "start" || _this.startButton.value == "resume"){//if timer should resume
+			if(_this.startButton.value == "start"){//if timer needs to read duration
+				_this.duration=createDuration(_this.hInput.value, _this.mInput.value, _this.sInput.value).totalMS;
+				_this.timeDisplay.innerHTML = getDurationAsString(_this.duration);
+				_this.toggleEditMode(_this);
+			}
+
+			_this.startButton.value = "pause";
+			_this.startDate = new Date();
+		}else if(_this.startButton.value == "pause"){//if timer needs to pause
+			_this.msOffset += new Date()-_this.startDate;
+			_this.startButton.value = "resume";
+			_this.startDate = null;
+		}else if(_this.startButton.value == "reset"){//if timer needs to reset
+			clearInterval(timerRingInterval);
+			_this.startButton.value = "start";
+			_this.msOffset = 0;
+			_this.startDate = null;
+			_this.toggleEditMode(_this);
+		}
+	}
+
+	//function that is called periodically to update the timer
 	tick(){
 		if(this.startDate != null){
 			let difference = new Date()-this.startDate;
 			let timeRemaining = this.duration-(difference+this.msOffset);
-			
+
 			if(timeRemaining > 0){
 				this.timeDisplay.innerHTML = getDurationAsString(timeRemaining);
 			} else {
 				this.timeDisplay.innerHTML = "Timer over!";
 				this.startButton.value = "reset";
-				console.log("asd?");
-				
-				
 			}
 
 			//ringing
@@ -82,87 +155,20 @@ class Timer{
 					timerRingInterval = setInterval(function(){
 						console.log("ring");
 						var audio = new Audio("pixbay-alarm-clock-short.mp3");
-						audio.play();//TODO make audio play only when nessacary 
-						
+						audio.play();//TODO make audio play only when nessacary
+
 					}, 1000);
 				}
 			}
 		}
 	}
-
-	constructor(){
-		//init element fragment
-		let element = timer_tmplt.content.cloneNode(true);
-		this.timer = element.querySelector(".timer");//the element that this timer object corresponds to
-		
-		//add element to dom
-		document.getElementById("timers").appendChild(element);
-		
-		//element references
-		this.xButton = this.timer.querySelector(".x-button");
-		this.startButton = this.timer.querySelector(".start-button");
-		this.timeDisplay = this.timer.querySelector(".time-display");
-		this.timeInput = this.timer.querySelector(".time-input");
-		this.hInput = this.timer.querySelector(".h-input");
-		this.mInput = this.timer.querySelector(".m-input");
-		this.sInput = this.timer.querySelector(".s-input");
-		
-		//default variable values
-		this.msOffset = 0;
-		this.startDate = null;
-		this.duration = 1000*60;
-		
-		//references to this object for eventlisteners
-		let _this = this;
-
-		//event listeners
-		this.xButton.addEventListener("click", function(){
-			if(_this.startButton.value == "start"){
-				mt_deleteTimer(_this);
-			}else{
-				_this.startButton.value = "start";
-				_this.msOffset = 0;
-				_this.startDate = null;
-				_this.timeInput.hidden = false;
-				_this.timeDisplay.hidden = true;
-			}
-		});
-
-		this.startButton.addEventListener("click", function(){
-			test();
-			if(_this.startButton.value == "start" || _this.startButton.value == "resume"){
-				
-				if(_this.startButton.value == "start"){
-					_this.duration=createDuration(_this.hInput.value, _this.mInput.value, _this.sInput.value).totalMS;
-					_this.timeDisplay.innerHTML = getDurationAsString(_this.duration);
-					_this.timeInput.hidden = true;
-					_this.timeDisplay.hidden = false;
-				}
-				
-				_this.startButton.value = "pause";
-				_this.startDate = new Date();
-			}else if(_this.startButton.value == "pause"){
-				_this.msOffset += new Date()-_this.startDate;
-				_this.startButton.value = "resume";
-				_this.startDate = null;
-			}else if(_this.startButton.value == "reset"){
-				clearInterval(timerRingInterval);
-				_this.startButton.value = "start";
-				_this.msOffset = 0;
-				_this.startDate = null;
-				_this.timeInput.hidden = false;
-				_this.timeDisplay.hidden = true;
-			}
-		});
-	}
-
 }
 
 //adds a timer
 function mt_addTimer(){
 	//create timer object
 	timers.push(new Timer());
-	
+
 	//set timertick interval if its not set already
 	if(timerTickInterval == null){
 		timerTickInterval = setInterval(function(){
@@ -180,7 +186,7 @@ function mt_deleteTimer(element){
 	//remove element from the timers array
 	let pos = timers.indexOf(element);
 	timers.splice(pos, 1);
-	
+
 	//delete from DOM
 	element.timer.parentNode.removeChild(element.timer);
 
