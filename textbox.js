ba14fa2548ce2ae728a9b0af1fea420008b6b8a7
Nolan Whitecotton <nolanwhitecotton@gmail.com>
2022-04-19 14:11:07 -0500

    Textbox: added a sort lines button
    This button splits the input at newlines sorts them, then updates the input.

--- textbox.js
+++ textbox.js
@@ -16,3 +16,20 @@ function tb_updateCharacterCounts(){
 	
 	document.getElementById("wordCount").innerHTML = wordCount;
 }
+
+//sorts all of the lines in the textbox alphabetically (does not ignore case)
+function tb_alphabetizeLines(){
+	let input = document.getElementById("textarea");
+	let lines = input.value.split("\n");
+	lines.sort();
+
+	let output = "";
+	for(let i=0; i<lines.length; i++){
+		output += lines[i];
+		if(i != lines.length-1){//if not the last line
+			output += "\n";
+		}
+	}
+
+	input.value = output;
+}
